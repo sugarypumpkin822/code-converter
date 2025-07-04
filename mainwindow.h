39a3167b4912b9bcccd21949072f131031be7e02
ryan allemand <giggatron.theraper@gmail.com>
2025-07-03 18:49:02 -0700

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -3,55 +3,19 @@
 
 #include <QMainWindow>
 #include <QDateTime>
-#include <QList>
-#include <QString>
+#include <QFont>
+#include <QColor>
+#include <QTimer>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-/**
- * @brief The MainWindow class provides the main GUI for CodeTranslator.
- * It includes advanced features such as translation history, settings, feedback, plugin management, and more.
- */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
-
-private slots:
-    /// Triggered when the Translate button is clicked
-    void on_translateButton_clicked();
-    /// Save output code to file
-    void saveOutput();
-    /// Load input code from file
-    void loadInput();
-    /// Copy output code to clipboard
-    void copyOutput();
-    /// Clear all input/output fields
-    void clearAll();
-    /// Show translation history dialog
-    void showHistory();
-    /// Send feedback dialog
-    void sendFeedback();
-    /// Manage plugins dialog
-    void managePlugins();
-    /// Show about dialog
-    void showAbout();
-    /// Show help dialog
-    void showHelp();
-    /// Save settings on close
-    void saveSettings();
-    /// Load settings on startup
-    void loadSettings();
-
-private:
-    Ui::MainWindow *ui;
-
-    /// Struct for translation history entries
     struct TranslationEntry {
         QString sourceLang;
         QString targetLang;
@@ -59,8 +23,45 @@ private:
         QString output;
         QDateTime timestamp;
     };
-    /// List of translation history entries
+
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+public slots:
+    void on_translateButton_clicked();
+    void saveOutput();
+    void loadInput();
+    void exportHistory();
+    void importHistory();
+    void copyOutput();
+    void clearAll();
+    void showHistory();
+    void showStats();
+    void sendFeedback();
+    void managePlugins();
+    void showAbout();
+    void showHelp();
+    void reportBug();
+    void setFont();
+    void setThemeColor();
+    void openOutputDirectory();
+    void showRandomTip();
+    void saveSettings();
+    void loadSettings();
+    void autoSave();
+
+protected:
+    void closeEvent(QCloseEvent *event) override {
+        saveSettings();
+        QMainWindow::closeEvent(event);
+    }
+
+private:
+    Ui::MainWindow *ui;
+    QTimer *autoSaveTimer;
+
+    // Static translation history (shared for all instances)
     static QList<TranslationEntry> translationHistory;
 };
 
-#endif // MAINWINDOW_H 
+#endif // MAINWINDOW_H
