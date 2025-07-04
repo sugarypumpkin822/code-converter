6158825eef9aa3b232c8e082c88c9895ad5020b1
ryan allemand <ryan4567dfr@outlook.com>
2025-07-03 18:25:17 -0700

    Add files via upload

---
+++ build/CMakeFiles/CodeTranslator_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CodeTranslator_autogen"
+  "CodeTranslator_autogen/include/ui_mainwindow.h"
+  "CodeTranslator_autogen/mocs_compilation.cpp"
+  "CodeTranslator_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/CodeTranslator_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
