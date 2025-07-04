96367755c294a6b520a7ad45abf0b718817b7684
ryan allemand <giggatron.theraper@gmail.com>
2025-07-03 18:43:48 -0700

    Delete build directory

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_CodeTranslator_FILE C:/Users/Ryan4/Downloads/CodeTranslator/build/CodeTranslator.exe)
-set(__QT_DEPLOY_TARGET_CodeTranslator_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_CodeTranslator_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
